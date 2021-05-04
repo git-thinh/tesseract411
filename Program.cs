e3338e42af7a1ab2f0d86d5ea78ee5b7661c03e9
Mr Thinh <33323583+git-thinh@users.noreply.github.com>
2021-05-04 21:09:33 +0700

    done

--- Program.cs
+++ Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Drawing;
-using System.Drawing.Imaging;
-using System.IO;
 using System.ServiceProcess;
 using System.Text;
 using System.Threading;
@@ -12,36 +10,12 @@ using Newtonsoft.Json;
 
 class Program
 {
-    static int m_port_write = 0;
-    static int m_port_read = 0;
-
-    #region [ CODE ]
-
+    static int __PORT_WRITE = 1000;
+    static int __PORT_READ = 1001;
     static RedisBase m_subcriber;
-    static bool _subscribe(string channel)
-    {
-        if (string.IsNullOrEmpty(channel)) return false;
-        channel = "<{" + channel + "}>";
-        try
-        {
-            StringBuilder sb = new StringBuilder();
-            sb.Append("*2\r\n");
-            sb.Append("$10\r\nPSUBSCRIBE\r\n");
-            sb.AppendFormat("${0}\r\n{1}\r\n", channel.Length, channel);
+    static bool __running = true;
 
-            byte[] buf = Encoding.UTF8.GetBytes(sb.ToString());
-            var ok = m_subcriber.SendBuffer(buf);
-            var lines = m_subcriber.ReadMultiString();
-            //Console.WriteLine("\r\n\r\n{0}\r\n\r\n", string.Join(Environment.NewLine, lines));
-            return ok;
-        }
-        catch (Exception ex)
-        {
-        }
-        return false;
-    }
-
-    static oTesseractRequest __ocrExecute(oTesseractRequest req, Bitmap image, RedisBase redis)
+    static oTesseractRequest __ocrExecute(oTesseractRequest req, Bitmap image)
     {
         PageIteratorLevel level = PageIteratorLevel.Word;
         switch (req.command)
@@ -93,7 +67,9 @@ class Program
                         req.ok = 1;
                         break;
                     default:
-                        var boxes = tes.GetSegmentedRegions(level).Select(x => new oTesseractBox(x)).ToArray();
+                        var boxes = tes.GetSegmentedRegions(level).Select(x =>
+                            string.Format("{0}_{1}_{2}_{3}", x.X, x.Y, x.Width, x.Height)).ToArray();
+                        req.output_format = "x_y_width_height";
                         req.output_text = string.Join("|", boxes.Select(x => x.ToString()).ToArray());
                         req.output_count = boxes.Length;
                         req.ok = 1;
@@ -108,14 +84,13 @@ class Program
     {
         oTesseractRequest r = null;
         string guid = Encoding.ASCII.GetString(buf);
-        var redis = new RedisBase(new RedisSetting(REDIS_TYPE.ONLY_READ, 1000));
+        var redis = new RedisBase(new RedisSetting(REDIS_TYPE.ONLY_READ, __PORT_WRITE));
         try
         {
             string json = redis.HGET("_OCR_REQUEST", guid);
             r = JsonConvert.DeserializeObject<oTesseractRequest>(json);
             Bitmap bitmap = redis.HGET_BITMAP(r.redis_key, r.redis_field);
-            if (bitmap != null)
-                r = __ocrExecute(r, bitmap, redis);
+            if (bitmap != null) r = __ocrExecute(r, bitmap);
         }
         catch (Exception ex)
         {
@@ -137,15 +112,10 @@ class Program
         }
     }
 
-    #endregion
-
     static void __startApp()
     {
-        if (m_port_write == 0) m_port_write = 1000;
-        if (m_port_read == 0) m_port_read = 1001;
-        m_subcriber = new RedisBase(new RedisSetting(REDIS_TYPE.ONLY_SUBCRIBE, 1001));
-        _subscribe("__TESSERACT_IN");
-
+        m_subcriber = new RedisBase(new RedisSetting(REDIS_TYPE.ONLY_SUBCRIBE, __PORT_READ));
+        m_subcriber.PSUBSCRIBE("__TESSERACT_IN");
         var bs = new List<byte>();
         while (__running)
         {
@@ -155,20 +125,16 @@ class Program
                 {
                     var buf = m_subcriber.__getBodyPublish("__TESSERACT_IN", bs.ToArray());
                     bs.Clear();
-                    new Thread(new ParameterizedThreadStart((o)
-                        => __executeBackground((byte[])o))).Start(buf);
+                    new Thread(new ParameterizedThreadStart((o) => __executeBackground((byte[])o))).Start(buf);
                 }
-
                 Thread.Sleep(100);
                 continue;
             }
-
             byte b = (byte)m_subcriber.m_stream.ReadByte();
             bs.Add(b);
         }
     }
 
-    static bool __running = true;
     static void __stopApp() => __running = false;
 
     #region [ SETUP WINDOWS SERVICE ]
